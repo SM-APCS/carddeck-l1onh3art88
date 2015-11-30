bc12548683708380b87c137b9d7d877f2dbc152c
Kurt <Kurt@10.4.2.43>
2015-11-30 10:55:11 -0600

    Code starter

---
+++ CardDeck/src/carddeck/CardDeck.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carddeck;
+
+/**
+ *
+ * @author Kurt
+ */
+public class CardDeck {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
