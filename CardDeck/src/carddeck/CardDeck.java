350ab57194a11569bb935f80a3d30f2895cb3ab5
l1onh3art88 <spenccheng@gmail.com>
2015-12-02 20:55:31 -0600

    Deck

--- CardDeck/src/carddeck/CardDeck.java
+++ CardDeck/src/carddeck/CardDeck.java
@@ -7,15 +7,41 @@ package carddeck;
 
 /**
  *
- * @author Kurt
+ * @author Spencer
  */
 public class CardDeck {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        private static Card[] cards;
+        int topCardIndex;
+        public CardDeck(){
+            cards = new Card[52];
+            int numberOfCard=0; 
+            for(int suit = 0; suit <= 3; suit++){
+                for(int value = 1; value <= 13; value++){
+                 cards[numberOfCard] = new Card(value, suit);
+                 numberOfCard++;
+                }
+            };
+        }
+        public CardDeck shuffle(CardDeck deck){
+            for(int i = 0; i<52; i++){
+                int rand = (int)(Math.random()*(i+1));
+                Card temp = deck.cards[i];
+                deck.cards[i] = deck.cards[rand];
+                deck.cards[rand] = temp;
+            }
+            return deck;
+        }
+        public void deal(){
+            for(int i = 0; i< 52; i++){
+            Card card = cards[i];
+            System.out.println(card);
+            }
+        }
+        
+    
     
 }
